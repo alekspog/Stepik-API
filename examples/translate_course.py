1990b2db6002ae69f3b60ae8e5f3c5497dd65c59
alekspog <aleksey.pogibelev@gmail.com>
2019-09-23 18:15:48 +0300

    init

---
+++ examples/translate_course.py
@@ -0,0 +1,107 @@
+# Run with Python 3
+# Saves all step texts from course into single HTML file.
+import requests
+
+# Enter parameters below:
+# 1. Get your keys at https://stepik.org/oauth2/applications/
+# (client type = confidential, authorization grant type = client credentials)
+client_id = "DKAOgdHrEaVBgshlsLllp9YTtRTXxUImQyVznrBY"
+client_secret = "EZIAoy31Q4V0YF8lDmHqgWOluVn5trNVM54XBgC2doi0zcdsYKT2vCeQc1ZcjR5AMD1QVTXzX9nmxT81AvlkNoA0Yo7Vuv2OHtVepHlEKblfit38eOfKEBPwGm0dhiTn"
+api_host = 'https://stepik.org'
+course_id = 1
+
+# 2. Get a token
+auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+response = requests.post('https://stepik.org/oauth2/token/',
+                         data={'grant_type': 'client_credentials'},
+                         auth=auth)
+token = response.json().get('access_token', None)
+if not token:
+    print('Unable to authorize with provided credentials')
+    exit(1)
+
+
+# 3. Call API (https://stepik.org/api/docs/) using this token.
+def fetch_object(obj_class, obj_id):
+    api_url = '{}/api/{}s/{}'.format(api_host, obj_class, obj_id)
+    response = requests.get(api_url,
+                            headers={'Authorization': 'Bearer ' + token}).json()
+    return response['{}s'.format(obj_class)][0]
+
+
+def fetch_objects(obj_class, obj_ids, keep_order=True):
+    objs = []
+    # Fetch objects by 30 items,
+    # so we won't bump into HTTP request length limits
+    step_size = 30
+    for i in range(0, len(obj_ids), step_size):
+        obj_ids_slice = obj_ids[i:i + step_size]
+        api_url = '{}/api/{}s?{}'.format(api_host, obj_class,
+                                         '&'.join('ids[]={}'.format(obj_id)
+                                                  for obj_id in obj_ids_slice))
+        response = requests.get(api_url,
+                                headers={'Authorization': 'Bearer ' + token}
+                                ).json()
+
+        objs += response['{}s'.format(obj_class)]
+    if (keep_order):
+        return sorted(objs, key=lambda x: obj_ids.index(x['id']))
+    return objs
+
+def translate(text: str):
+
+    return text.lower()
+
+def translate_step(step_id):
+    obj_class = 'step-source'
+    obj_id = step_id
+    api_url = '{}/api/{}s/{}'.format(api_host, obj_class, obj_id)
+    response = requests.get(api_url,
+                            headers={'Authorization': 'Bearer ' + token}).json()
+    print(response)
+    name = response[f'{obj_class}s'][0]['block']['name']
+    text = response[f'{obj_class}s'][0]['block']['text']
+    lesson_id = response[f'{obj_class}s'][0]['lesson']
+    position = response[f'{obj_class}s'][0]['position']
+    text = translate(text)
+    if name == 'text':
+        data = {
+            'stepSource': {
+                'block': {
+                    'name': name,
+                    'text': text,
+                },
+                'lesson': lesson_id,
+                'position': position
+            }
+        }
+    else:
+        data = {
+            'stepSource': {
+                'block': {
+                    'name': name,
+                    'text': text,
+                },
+                'lesson': lesson_id,
+                'position': position
+            }
+        }
+    response = requests.put(api_url,
+                 headers={'Authorization': 'Bearer ' + token},
+                 json=data
+                 )
+    print(response)
+
+# course = fetch_object('course', course_id)
+# sections = fetch_objects('section', course['sections'])
+#
+# unit_ids = [unit for section in sections for unit in section['units']]
+# units = fetch_objects('unit', unit_ids)
+#
+# lesson_ids = [unit['lesson'] for unit in units]
+# lessons = fetch_objects('lesson', lesson_ids)
+#
+# step_ids = [step for lesson in lessons for step in lesson['steps']]
+# steps = fetch_objects('step', step_ids)
+
+translate_step(822047)
